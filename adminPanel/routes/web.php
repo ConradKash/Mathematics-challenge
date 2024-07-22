18a100aeed41ddd5e113335d1bb9d389acbc0fcf
MilbertCatherine <mmbalire04@gmail.com>
2024-07-22 22:40:34 +0300

    Cathy done

--- adminPanel/routes/web.php
+++ adminPanel/routes/web.php
@@ -35,7 +35,7 @@ Route::delete('/schools/{id}', 'App\Http\Controllers\SchoolsController@delete')-
 Route::group(['middleware' => 'auth'], function () {
 	Route::resource('user', 'App\Http\Controllers\UserController', ['except' => ['show']]);
 	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'App\Http\Controllers\ProfileController@edit']);
-	Route::patch('profile', ['as' => 'profile.update', 'uses' => 'App\Http\Controllers\ProfileController@update']);
+	Route::patch('profile', ['as' => 'profile.update',  'uses' => 'App\Http\Controllers\ProfileController@update']);
 	Route::patch('profile/password', ['as' => 'profile.password', 'uses' => 'App\Http\Controllers\ProfileController@password']);
 });
 
